904376fcb7f8043d977bd239eb863e7c08e7f36c
bisenteherradura <vincentaisner12@gmail.com>
2026-02-09 10:22:00 +0800

    added routing and a user info placeholder

---
+++ src/pages/MainDashboard/SubmitTicket.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function SubmitTicket(){
+  return(
+    <div className="wrapper">
+      <div className="card">
+        <h1>Submit Ticket</h1>
+      </div>
+    </div>
+  );
+}
+
+export default SubmitTicket;
